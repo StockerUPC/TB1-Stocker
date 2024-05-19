565f49621090e34ada3765fe952188468df4207d
RodrLH <u202212338@upc.edu.pe>
2024-05-18 21:17:42 -0500

    TB1

---
+++ vue-project/src/App-SignIn.vue
@@ -0,0 +1,15 @@
+<script setup>
+import SignInForm from '../src/components/SignInPage.vue'
+import LandingHeader from "@/components/LandingHeader.vue";
+</script>
+
+<template>
+<main>
+  <LandingHeader/>
+  <SignInForm/>
+</main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-project/src/singIn.js
@@ -0,0 +1,5 @@
+import './assets/base.css'
+import { createApp } from 'vue'
+import AppSignIn from './App-SignIn.vue'
+
+createApp(AppSignIn).mount('#app-signin')
